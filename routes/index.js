66bc254b585860e0fbd29e1348d47bfd0ec93b27
chitrakakkar <chitrakakkar@yahoo.co.in>
2017-03-02 18:52:43 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var apod = require('../helpers/apod');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/*get a picture from
+
+ */
+router.get('/fetch_picture', function(req, res, next){
+
+  if (req.query.today) {
+
+    apod(function(data, error){
+      if (error) {
+        return res.render('apod_error', { error : error.message });
+      }
+      console.log('ROUTE SAYS' + JSON.stringify(data));
+      return res.render('picture', { apod : data });
+    }, true);
+
+  }
+  else if (req.query.random) {
+
+    apod(function(data, error) {
+      if (error) {
+        return res.render('apod_error', { error : error.message });
+      }
+
+      //console.log('ROUTE SAYS' + JSON.stringify(data));
+      return res.render('picture', { apod : data });
+    });
+
+  } else {
+    next();  // Send to next route handler.
+    // Since we haven't defined one, this will end up at the 404 error handler
+  }
+
+});
+module.exports = router;
