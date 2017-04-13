50d048d353331978adb45485e709e9aeb07f4f22
chitrakakkar <chitrakakkar@yahoo.co.in>
2017-04-13 18:55:59 -0500

    App.js having problem

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
+var session = require('express-sessions');
+var MongoDBStore = require('connect-mongodb-session')(session);
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var favorites = require('./routes/favorites');
 
 var app = express(); // creating a new express object- in this case, a new application; app main-entry
 
@@ -31,8 +33,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configure sessions, and session storage in MongoDB
+var mongo_pw = process.env.MONGO_PW;
+var url = 'mongodb://chitrakakkar:' + mongo_pw + '@localhost:27017/helloSessions?authSource=admin';
+
+var store = new MongoDBStore({
+    uri : url,
+    collection : 'sessions'
+}, function(error) {
+    // todo deal with error connection
+    if (error) console.log(error)
+});
+
+app.use(session({
+    secret: "put a random string here",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}));
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/favorites', favorites);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
